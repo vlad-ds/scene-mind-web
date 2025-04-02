f90e614e25de6f5f20ab23aa9c2bcf923678024d
vladgheorghe <vlad.proex@gmail.com>
2025-04-02 09:37:01 +0200

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Copy the "img" directory to the output
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("docs");
+  
+  // Copy the "css" directory to the output
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "../_includes",
+      layouts: "../_includes"
+    }
+  };
+};
